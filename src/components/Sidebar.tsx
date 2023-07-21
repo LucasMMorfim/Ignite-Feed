5a63f9f7113e0af640b823d11670d5dc00cebbb7
Lucas Morfim <lucas.morfim@hotmail.com>
2023-07-21 09:59:54 -0300

    update

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { Avatar } from "./Avatar";
+import styles from "./Sidebar.module.css";
+import { PencilLine } from "phosphor-react";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1534423861386-85a16f5d13fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"/>
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/LucasMMorfim.png"/>
+
+        <strong>Lucas Morfim</strong>
+        <span>Web Development</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
